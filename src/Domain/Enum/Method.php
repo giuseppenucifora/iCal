15eec32a8423a450c09c48cd617d635aeb334605
Gregorio Grifo <g.giorgino@grifomultimedia.it>
2021-09-09 13:39:41 +0200

    Added request method

--- src/Domain/Enum/Method.php
+++ src/Domain/Enum/Method.php
@@ -14,6 +14,7 @@ namespace Eluceo\iCal\Domain\Enum;
 final class Method
 {
     private static ?self $publish = null;
+    private static ?self $request = null;
     private static ?self $cancel = null;
 
     private string $method;
@@ -28,6 +29,11 @@ final class Method
     public static function PUBLISH(): self
     {
         return self::$publish = self::$publish ?? new Method('PUBLISH');
+    }
+    
+    public static function REQUEST(): self
+    {
+        return self::$request = self::$request ?? new Method('REQUEST');
     }
 
     public static function CANCELLED(): self
